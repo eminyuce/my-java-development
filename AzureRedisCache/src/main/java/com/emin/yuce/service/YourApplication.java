cee16dc3878263c11251dcb333a12346e528537f
eminyuce <eminyuce@gmail.com>
2023-12-26 17:06:45 -0500

    YourApplication

--- AzureRedisCache/src/main/java/com/emin/yuce/service/YourApplication.java
+++ AzureRedisCache/src/main/java/com/emin/yuce/service/YourApplication.java
@@ -29,6 +29,27 @@ public class YourApplication {
     }
 }
 
+    public static void IOUtils.toString(String[] args) {
+        try {
+            // Specify the file path relative to the "resources" folder
+            String filePath = "file.txt";
+
+            // Use ClassLoader to get the resource stream
+            InputStream inputStream = FileReadingExample.class.getClassLoader().getResourceAsStream(filePath);
+
+            if (inputStream != null) {
+                // Read the content of the file using IOUtils
+                String fileContent = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+
+                // Print or process the file content
+                System.out.println(fileContent);
+            } else {
+                System.err.println("File not found: " + filePath);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 <dependencies>
 <dependency>
 <groupId>org.springframework.boot</groupId>
