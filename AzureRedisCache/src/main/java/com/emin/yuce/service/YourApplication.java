e64a197ecbabcb37b5842ff22c8f85d736ae9f6f
eminyuce <eminyuce@gmail.com>
2023-12-26 12:05:03 -0500

    YourApplication

---
+++ AzureRedisCache/src/main/java/com/emin/yuce/service/YourApplication.java
@@ -0,0 +1,30 @@
+package com.emin.yuce.service;
+
+
+@SpringBootApplication
+public class YourApplication {
+
+    public static void main(String[] args) {
+        // Get the application context
+        ConfigurableApplicationContext context = SpringApplication.run(YourApplication.class, args);
+
+        // Get the FileService bean from the application context
+        FileService fileService = context.getBean(FileService.class);
+
+        try {
+            // Call a method on the FileService bean
+            List<String> fileContents = fileService.getAllFilesContentInCustomerFolder();
+
+            // Do something with the file contents
+            for (String content : fileContents) {
+                System.out.println(content);
+            }
+        } catch (IOException e) {
+            // Handle exceptions
+            e.printStackTrace();
+        } finally {
+            // Close the application context
+            context.close();
+        }
+    }
+}
